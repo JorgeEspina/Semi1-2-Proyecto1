4b33c0217c93fa1c2561d93a5ab21df9849e5e78
Jorge Espina <canchemolinas@gmail.com>
2021-08-24 00:47:22 -0600

    Vista Login y Dashboard

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { PrincipalComponent } from './component/principal/principal.component';
+import { RegistroComponent } from './component/registro/registro.component';
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent, PrincipalComponent, RegistroComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ Frontend/src/app/component/principal/principal.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css'],
+})
+export class PrincipalComponent implements OnInit {
+  constructor() {
+    this.loadScripts();
+  }
+
+  ngOnInit(): void {}
+  loadScripts() {
+    const dynamicScripts = ['../../../assets/js/script.js'];
+    for (let i = 0; i < dynamicScripts.length; i++) {
+      const node = document.createElement('script');
+      node.src = dynamicScripts[i];
+      node.type = 'text/javascript';
+      node.async = false;
+      node.charset = 'utf-8';
+      document.getElementsByTagName('head')[0].appendChild(node);
+    }
+  }
+}
