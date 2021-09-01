2c3dd6812da44a32e50630fd753347b568100606
JohanChan <johanleo97@gmail.com>
2021-08-31 19:22:58 -0600

    Endpoints Registro y Login

---
+++ backend/db_creds.js
@@ -0,0 +1,8 @@
+let db_credentials = {
+    host: "ustoragedb.ci0shyuctz7e.us-east-2.rds.amazonaws.com",
+    port: 3306,
+    user: "admin",
+    password: "S3minari02doSemestre*",
+    database: "storagedb"
+}
+module.exports = db_credentials;
