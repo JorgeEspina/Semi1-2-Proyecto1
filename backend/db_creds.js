f31a65f0229445822ffdb76246e91da18dfe12de
JorgeEspina <canchemolinas@JorgeEspina.noreply.github.com>
2021-09-13 00:07:59 -0600

    update db_creds

--- backend/db_creds.js
+++ backend/db_creds.js
@@ -1,8 +1,8 @@
 let db_credentials = {
-    host: "ustoragedb.ci0shyuctz7e.us-east-2.rds.amazonaws.com",
+    host: "ustorage-db.ci0shyuctz7e.us-east-2.rds.amazonaws.com",
     port: 3306,
     user: "admin",
     password: "S3minari02doSemestre*",
-    database: "storagedb"
+    database: "ustorage"
 }
 module.exports = db_credentials;
